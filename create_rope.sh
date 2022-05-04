0f94e00bc3d996c90dcf93df24820c554d267479
NatashaPinegina <natasha_pinegina@mail.ru>
2022-05-04 11:22:11 +0300

    Исправлен скрипт сборки, клонируется в домашнюю директорию

--- create_rope.sh
+++ create_rope.sh
@@ -1,16 +1,16 @@
-mkdir -p ~/cop/cs/workspace/src
-cd ~/cop/cs/workspace/src
+mkdir -p ~/CS/workspace/src
+cd ~/CS/workspace/src
 catkin_init_workspace
 catkin_create_pkg lab1
 cd lab1
 sudo rm CMakeLists.txt package.xml
-cd ~/cop/cs/Rope
+cd ~/CS/Rope
 cp CMakeLists.txt ../workspace/src/lab1/
 cp generatesdf.cpp ../workspace/src/lab1/
 cp newRope.cpp ../workspace/src/lab1/
 cp rope.h ../workspace/src/lab1/
 cp package.xml ../workspace/src/lab1/
-cd ~/cop/cs/workspace/src/lab1
+cd ~/CS/workspace/src/lab1
 cmake .
 make
 cd devel/lib/lab1/
